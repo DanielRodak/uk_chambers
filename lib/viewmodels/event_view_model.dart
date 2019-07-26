22799ab8474e1b7b1cdc9da026c7bf59c63242ac
danielr <daniel.rodak@untitledkingdom.com>
2019-07-26 21:21:52 +0200

    implemented EventsRepoModel

--- lib/viewmodels/event_view_model.dart
+++ lib/viewmodels/event_view_model.dart
@@ -1,13 +1,20 @@
 import 'package:meta/meta.dart';
+import 'package:uk_chambers/data/chamber.dart';
 
 class EventsRepoModel {
   final String id;
-  final String name;
-  final String imageUrl;
+  final Chamber chamber;
+  final String username;
+  final int dateStart;
+  final int dateEnd;
+  final bool cycle;
 
   EventsRepoModel({
     @required this.id,
-    @required this.name,
-    @required this.imageUrl,
+    @required this.chamber,
+    @required this.username,
+    @required this.dateStart,
+    @required this.dateEnd,
+    @required this.cycle,
   });
 }
